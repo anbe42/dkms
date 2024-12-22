4cce7380dadbc6891f514f0bfc55cd63839c52f4
Andreas Beckmann <anbe@debian.org>
2024-12-23 00:58:06 +0100

    noisy test: use a failing clean command

--- run_test.sh
+++ run_test.sh
@@ -1634,7 +1634,9 @@ pre_build: line 3
 pre_build: line 4/stderr
 pre_build: line 5
 
-Cleaning build area... done.
+Cleaning build area...(bad exit status: 2)
+Failed command:
+make clean
 Building module(s)... done.
 ${SIGNING_MESSAGE_noisy}
 Running the post_build script:
@@ -1644,7 +1646,9 @@ post_build: line 2/stderr
 post_build: line 3
 post_build: line 4/stderr
 post_build: line 5
-Cleaning build area... done.
+Cleaning build area...(bad exit status: 2)
+Failed command:
+make clean
 
 
 Running the pre_install script:
@@ -1723,7 +1727,9 @@ pre_build: line 3
 pre_build: line 4/stderr
 pre_build: line 5
 
-Cleaning build area... done.
+Cleaning build area...(bad exit status: 2)
+Failed command:
+make clean
 Building module(s)... done.
 ${SIGNING_MESSAGE_noisy}
 Running the post_build script:
@@ -1733,7 +1739,9 @@ post_build: line 2/stderr
 post_build: line 3
 post_build: line 4/stderr
 post_build: line 5
-Cleaning build area... done.
+Cleaning build area...(bad exit status: 2)
+Failed command:
+make clean
 
 
 Running the pre_install script:
